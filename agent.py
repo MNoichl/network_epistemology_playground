d0a141f9a33b4943790590f28d79292426bad926
IgnacioOQ <ignacioojea@gmail.com>
2024-05-30 15:37:00 +0200

    notes update

--- agent.py
+++ agent.py
@@ -80,8 +80,6 @@ class Agent:
             self.n_success, self.n_experiments = self.uncertainty_problem.experiment(
                 n_experiments
             )
-            self.accumulated_successes += self.n_success
-            self.accumulated_failures += (self.n_experiments-self.n_success)
         else:
             self.n_success = 0
             self.n_experiments = 0
@@ -108,9 +106,11 @@ class Agent:
         self.credence = 1 / (
             1 + likelihood_ratio_credence * likelihood_ratio_evidence_given_probability
         )
-        self.credence_history.append(self.credence[0])
+        self.credence_history.append(self.credence)
 
     def beta_update(self,n_success,n_experiments):
+        self.accumulated_successes += n_success
+        self.accumulated_failures += (n_experiments-n_success)
         p_new_better = 0.5 + self.uncertainty_problem.uncertainty
         p_new_worse = 0.5 - self.uncertainty_problem.uncertainty   
         mean, var= beta.stats(self.accumulated_successes, self.accumulated_failures, moments='mv')
@@ -166,7 +166,7 @@ class Agent:
         self.credence = p_new_better_given_E * p_post_E + p_new_worse_given_E * (
             1 - p_post_E
         )  # Jeffrey's Rule # P'(H) = P(H|E) P'(E) + P(H|~E) P'(~E)#
-        self.credence_history.append(self.credence[0])
+        self.credence_history.append(self.credence)
     
 
 class Bandit:
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 * Beta-distributions
     * We implemented the behaviour of the agent by following the highest mean. Other options could be to take the mode or to sample from the beta-distributions
 * assymmetry in experimental behaviour
+
+* Something seems fishy because if you look at change in self.credence by state for each agent, they dont seem to change.
